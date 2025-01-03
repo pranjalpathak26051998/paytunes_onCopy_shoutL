50efd3dd6a5cf9f9e563a585112fece3a2a936fa
Pranjal-Paytunes1432 <pranjal.p@paytunes.in>
2025-01-03 11:10:56 +0530

    new push

--- test/ad_manager/ad_manager.js
+++ test/ad_manager/ad_manager.js
@@ -8,6 +8,8 @@ const path = require('path');
     let website_url = 'https://staging-app.shoutl.com/';
     let stg_username = 'pranjal.p+staging_Pranjal@paytunes.in';
     let stg_password = 'User@123';
+    let text = '';
+    let newtext = '';
 
     if (!stg_username || !stg_password) {
         console.log('Please provide valid staging credentials');
@@ -62,41 +64,63 @@ const path = require('path');
             })
             //click on the submit button to submit
             it('should click on the submit button', async function () {
-                await driver.wait(until.elementLocated(By.xpath("//button[@type='submit']")),3000);
+                await driver.wait(until.elementLocated(By.xpath("//button[@type='submit']")), 3000);
                 await driver.findElement(By.xpath("//button[@type='submit']")).click();
                 await driver.sleep(2000);
                 await takeScreenshot(driver, 'after_submit.png');
             });
             //cross the single form appearing on the screen
             //div[@class='relative']/span
-            it('should click to cut the form', async function (){
+            it('should click to cut the form', async function () {
                 await driver.sleep(5000);
-                await driver.wait(until.elementLocated(By.xpath("//div[@class='relative']/span")),10000);
+                await driver.wait(until.elementLocated(By.xpath("//div[@class='relative']/span")), 10000);
                 await driver.findElement(By.xpath("//div[@class='relative']/span")).click();
-                
+
                 await takeScreenshot(driver, 'after_cut.png');
             });
 
             //click on the ad manager on the left nav bar 
             //a[@href='/ad-manager']
             it('should click on the ad manager on the left nav bar', async () => {
-                await driver.wait(until.elementLocated(By.xpath("//a[@href='/ad-manager']")),5000);
+                await driver.wait(until.elementLocated(By.xpath("//a[@href='/ad-manager']")), 5000);
                 await driver.findElement(By.xpath("//a[@href='/ad-manager']")).click();
                 console.log("Clicked on the ad manager on the left navbar successfully");
                 await driver.sleep(2000);
                 await takeScreenshot(driver, 'ad-manager.png');
             });
-            let audio_text_xpath = `//div[@class='react-multi-carousel-list carousel-container ']/ul/li/div[@class='m-7']/a/label`;
-            // let audio_text_xpath = `//*[@id="root"]/div[2]/div[2]/main/div/div/div[2]/section[1]/div/div[1]/ul/li[7]/div/a/label`;
+            // let audio_text_xpath = `//div[@class='react-multi-carousel-list carousel-container ']/ul/li/div[@class='m-7']/a/label`;
+            let audio_text_xpath = `// div[@class='react-multi-carousel-list carousel-container ']/ul/li[@class='react-multi-carousel-item react-multi-carousel-item--active ']/div[@class='m-7']/a/label`;
             //get Text from create audio following the xpath
             it('should get the text from create audio', async () => {
-                await driver.wait(until.elementLocated(By.xpath(audio_text_xpath)),5000);
-                let text = await driver.findElement(By.xpath(audio_text_xpath)).getText();
+                await driver.wait(until.elementLocated(By.xpath(audio_text_xpath)), 5000);
+                text = await driver.findElement(By.xpath(audio_text_xpath)).getText();
                 console.log(`Text from create audio: ${text}`);
             });
 
+            it('compare the text and take the action accordingly', async () => {
+                newtext = text;
+                if (newtext === 'Create audio now!') {
+                    await driver.wait(until.elementLocated(By.xpath(audio_text_xpath)), 5000);
+                    await driver.findElement(By.xpath(audio_text_xpath)).click();
+                    await driver.sleep(5000);
+                    await takeScreenshot(driver, 'createAudioPage.png');
+                    
+                    console.log('clicked on the Create audio now button successfully');
+                    // it('Click to create audio now!', async () => {
+                       
+                    // })                   
+                    
+                }
+                else { console.log('Create audio not found') };
+            });
 
-            
+
+
+            // div[@class='react-multi-carousel-list carousel-container ']/ul/li[@class='react-multi-carousel-item react-multi-carousel-item--active ']/div[@class='m-7']/a/label[text()='Create audio now!']
+            // [text()='Create audio now!']
+            // div[@class='react-multi-carousel-list carousel-container ']/ul/li[@class='react-multi-carousel-item react-multi-carousel-item--active ']/div[@class='m-7']/a/label
+
+
 
         } catch (error) {
             console.error(error.message);
